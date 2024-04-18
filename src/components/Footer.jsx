98c5016ddb910afaf025d35e60859f6119e56e78
Kaique colella <99101800+kaiquecolella@users.noreply.github.com>
2024-04-18 03:39:10 -0300

    Descrição

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className='container d-flex flex-column flex-lg-row  justify-content-between align-items-center align-items-lg-start'>
             <div className='col-6'>
                 <img className='col-3' src={Logo} alt="" />
-                <p>Villares Auto Partes - Sua Fonte Confiável para Peças Automotivas Inovadoras e Sustentáveis. Há mais de duas décadas, reinventamos o desmanche, proporcionando soluções acessíveis e ecológicas para veículos nacionais e importados. Junte-se a nós , onde tradição encontra inovação, e a qualidade preserva a performance do seu carro.</p>
+                <p>villares auto parts - Sua Fonte Confiável para Peças Automotivas Inovadoras e Sustentáveis. Há mais de duas décadas, reinventamos o desmanche, proporcionando soluções acessíveis e ecológicas para veículos nacionais e importados. Junte-se a nós , onde tradição encontra inovação, e a qualidade preserva a performance do seu carro.</p>
             </div>
             <div className='col-6 col-lg-3 text-lg-end'>
                 <h3>Contato</h3>
